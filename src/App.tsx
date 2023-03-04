2b4a8f05804a3ae9c62106f92f339a2a6e37e407
veric <verich@mailru>
2023-03-05 01:32:51 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import {Header} from "./ui/Headet";
 import Container from '@mui/material/Container';
 import {SignIn} from "./ui/SignIn";
+import SignUp from "./ui/SignUp";
+import SignUpMain from "./ui/SignUpMain";
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <SignIn/>
+            <SignUpMain/>
            {/* <Container maxWidth={false}  sx={{ minHeight:'calc(100vh - 68.5px)', width: '100vw'}} >
 
             </Container>*/}
